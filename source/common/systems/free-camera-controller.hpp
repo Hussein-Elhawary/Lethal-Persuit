c5055c8f9d2c2c20effe93615af178e38e867cc6
Hussein Elhawary <husseinelhawary@gmail.com>
2024-12-10 18:20:58 +0200

    made player look around with out mouse click and adjusted rotation sens

--- source/common/systems/free-camera-controller.hpp
+++ source/common/systems/free-camera-controller.hpp
@@ -20,12 +20,13 @@ namespace our
     // For more information, see "common/components/free-camera-controller.hpp"
     class FreeCameraControllerSystem {
         Application* app; // The application in which the state runs
-        bool mouse_locked = false; // Is the mouse locked
+        bool mouse_locked = true; // Is the mouse locked
 
     public:
         // When a state enters, it should call this function and give it the pointer to the application
         void enter(Application* app){
             this->app = app;
+            app->getMouse().lockMouse(app->getWindow());
         }
 
         // This should be called every frame to update all entities containing a FreeCameraControllerComponent 
@@ -47,14 +48,14 @@ namespace our
             //printf("Entity: %s\n", entity->name.c_str());
 
             // If the left mouse button is pressed, we lock and hide the mouse. This common in First Person Games.
-            if(app->getMouse().isPressed(GLFW_MOUSE_BUTTON_1) && !mouse_locked){
-                app->getMouse().lockMouse(app->getWindow());
-                mouse_locked = true;
-            // If the left mouse button is released, we unlock and unhide the mouse.
-            } else if(!app->getMouse().isPressed(GLFW_MOUSE_BUTTON_1) && mouse_locked) {
-                app->getMouse().unlockMouse(app->getWindow());
-                mouse_locked = false;
-            }
+            //if(app->getMouse().isPressed(GLFW_MOUSE_BUTTON_1) && !mouse_locked){
+            //    app->getMouse().lockMouse(app->getWindow());
+            //    mouse_locked = true;
+            //// If the left mouse button is released, we unlock and unhide the mouse.
+            //} else if(!app->getMouse().isPressed(GLFW_MOUSE_BUTTON_1) && mouse_locked) {
+            //    app->getMouse().unlockMouse(app->getWindow());
+            //    mouse_locked = false;
+            //}
 
             // We get a reference to the entity's position and rotation
             glm::vec3& position = entity->localTransform.position;
@@ -62,11 +63,11 @@ namespace our
 
             // If the left mouse button is pressed, we get the change in the mouse location
             // and use it to update the camera rotation
-            if(app->getMouse().isPressed(GLFW_MOUSE_BUTTON_1)){
-                glm::vec2 delta = app->getMouse().getMouseDelta();
-                rotation.x -= delta.y * controller->rotationSensitivity; // The y-axis controls the pitch
-                rotation.y -= delta.x * controller->rotationSensitivity; // The x-axis controls the yaw
-            }
+            //if(app->getMouse().isPressed(GLFW_MOUSE_BUTTON_1)){
+            glm::vec2 delta = app->getMouse().getMouseDelta();
+            rotation.x -= delta.y * controller->rotationSensitivity; // The y-axis controls the pitch
+            rotation.y -= delta.x * controller->rotationSensitivity; // The x-axis controls the yaw
+            //}
 
             // We prevent the pitch from exceeding a certain angle from the XZ plane to prevent gimbal locks
             if(rotation.x < -glm::half_pi<float>() * 0.99f) rotation.x = -glm::half_pi<float>() * 0.99f;
